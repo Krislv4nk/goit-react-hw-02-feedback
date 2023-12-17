d8392f2301829d1d5121dff9527cbbc7bb7653d6
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2023-12-17 12:02:43 +0100

    feedback app

---
+++ src/components/feedbackOptions/feedbackOptions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './feedbackOptions.module.css';
+
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map(option => (
+      <button key={option} onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
---
+++ src/components/statistics/statistics.jsx
@@ -0,0 +1,30 @@
+   import React from 'react';
+import css from './statistics.module.css';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <ul>
+      <li>
+        <p>Good: {good}</p>
+      </li>
+      <li>
+        <p>Neutral: {neutral}</p>
+      </li>
+      <li>
+        <p>Bad: {bad}</p>
+      </li>
+      <li>
+        <p>Total: {total}</p>
+      </li>
+      <li>
+        <p>Positive feedback: {positivePercentage}%</p>
+      </li>
+    </ul>
+  );
+};
